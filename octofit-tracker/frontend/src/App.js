a8d23130a91f8ec5ed10346ea4e023cb0d072294
Everlyse Tay <etay@palo-it.com>
2025-11-24 16:35:30 +0800

    feat: update UI elements with new logo, enhanced styles, and improved layout

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -13,7 +13,10 @@ function App() {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
+            <Link className="navbar-brand" to="/">
+              <img src="/octofitapp-small.png" alt="OctoFit Logo" className="navbar-logo" />
+              OctoFit Tracker
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
